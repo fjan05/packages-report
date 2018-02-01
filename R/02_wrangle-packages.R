9bb64e493948c7fd1a4aab3d7482356d09074242
Francisco Aguirre <fjan05@gmail.com>
2018-02-01 11:53:50 -0800

    ## Second excercise

--- R/02_wrangle-packages.R
+++ R/02_wrangle-packages.R
@@ -4,19 +4,27 @@
 ## filter out packages in the default library
 ## keep variables Package and Built
 ## if you use dplyr, code like this will work:
+
 apt <- ipt %>%
   filter(LibPath == .libPaths()[1]) %>%
   select(Package, Built)
 
 ## write this new, smaller data frame to data/add-on-packages.csv
 
+write_csv(apt,here("data","add-on-packages.csv"))
+
+
 ## make a frequency table of package by the version in Built
 ## if you use dplyr, code like this will work:
+
 apt_freqtable <- apt %>%
   count(Built) %>%
   mutate(prop = n / sum(n))
 
 ## write this data frame to data/add-on-packages-freqtable.csv
+
+write_csv(apt_freqtable,here("data","add-on-packages-freqtable.csv"))
+
 ## YES overwrite the files that are there now
 ## they came from me (Jenny)
 ## they are just examples
